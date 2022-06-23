57944d507e471549d05cdb27e1140f5cf18c22bb
zhengmin <360475097@qq.com>
2022-06-23 18:18:02 +0800

    完成加载obj文件的示例

--- src/chapter/08/LoadObj.jsx
+++ src/chapter/08/LoadObj.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
-import { initStats, initCamera, initRenderer, initTrackballControls } from "../../utils";
+import { initCamera, initRenderer } from "../../utils";
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import objFile from "../../assets/models/pinecone/pinecone.obj";
 import BaseScene from "../../utils/baseScene";
 
 function LoadObj() {
@@ -18,8 +17,14 @@ function LoadObj() {
     let scene = new BaseScene(camera, renderer);
     camera.lookAt(new THREE.Vector3(0, 15, 0));
 
+    const material = new THREE.MeshLambertMaterial({ color: 0x5c3a21 });
+
     const loader = new OBJLoader();
-    loader.load(objFile, (mesh) => {
+    loader.load("/src/assets/models/pinecone/pinecone.obj", (mesh) => {
+      mesh.children.forEach((child) => {
+        child.material = material;
+      })
+      mesh.scale.set(120, 120, 120);
       scene.render(mesh, camera);
     });
   }
--- src/utils/baseScene.js
+++ src/utils/baseScene.js
@@ -1,8 +1,7 @@
 import * as THREE from "three";
 import { initStats, initTrackballControls } from "./index";
 
-
-function BaseScene(providedCamera, renderer, updateMesh) {
+function BaseScene(providedCamera, renderer, shouldAddLights, updateMesh) {
   self = this;
 
   this.scene = new THREE.Scene();
@@ -11,12 +10,11 @@ function BaseScene(providedCamera, renderer, updateMesh) {
   this.camera = providedCamera;
   this.updateMesh = updateMesh;
   this.renderer = renderer;
+  this.withLights = (shouldAddLights !== undefined) ? shouldAddLights : true;
 
   this.trackballControls = initTrackballControls(this.camera, this.renderer);
 
   this.render = (mesh, camera) => {
-    console.log("this---", this);
-    console.log("mesh---", mesh);
     this.scene.add(mesh);
     this.camera = camera;
     this.mesh = mesh;
@@ -34,6 +32,31 @@ function BaseScene(providedCamera, renderer, updateMesh) {
 
     self.renderer.render(self.scene, self.camera);
   }
+
+  this._addLights = function () {
+    let keyLight = new THREE.SpotLight(0xffffff);
+    keyLight.position.set(0, 80, 80);
+    keyLight.intensity = 2;
+    keyLight.lookAt(new THREE.Vector3(0, 15, 0));
+    keyLight.castShadow = true;
+    keyLight.shadow.mapSize.height = 4096;
+    keyLight.shadow.mapSize.width = 4096;
+    this.scene.add(keyLight);
+
+    let backlight1 = new THREE.SpotLight(0xaaaaaa);
+    backlight1.position.set(150, 40, -20);
+    backlight1.intensity = 0.5;
+    backlight1.lookAt(new THREE.Vector3(0, 15, 0));
+    this.scene.add(backlight1);
+
+    let backlight2 = new THREE.SpotLight(0xaaaaaa);
+    backlight2.position.set(-150, 40, -20);
+    backlight2.intensity = 0.5;
+    backlight2.lookAt(new THREE.Vector3(0, 15, 0));
+    this.scene.add(backlight2);
+  }
+
+  if (this.withLights) this._addLights();
 }
 
 export default BaseScene;
